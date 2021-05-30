31a509d0aad205fd57837312e25c14fb59587103
codeholic08 <48811879+codeholic08@users.noreply.github.com>
2021-05-30 19:41:55 +0530

    Add files via upload

---
+++ Face_Detection.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+img = cv2.imread("tim.jpg", 1)
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5)
+
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+cv2.imshow("Face",img)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
